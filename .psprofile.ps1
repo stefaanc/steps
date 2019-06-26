85432442255a335c7a21d69034dd36069b8a6259
Stefaan Coussement <stefaan.coussement@gmail.com>
2019-06-26 14:23:00 +0100

    update .gitignore

--- .psprofile.ps1
+++ .psprofile.ps1
@@ -1,4 +1,4 @@
-$ROOT = "$HOME\steps"
+$ROOT = "$HOME\Projects\steps"
 
 $env:ROOT = "$ROOT"
 $env:PATH = "$env:PATH;$ROOT\scripts"
@@ -12,9 +12,3 @@ Apply-PSConsoleSettings $ROOT\.psconsole.json
 $e = [char]27
 $env:STEPS_COLORS = "$e[38;5;45m,$e[92m,$e[93m,$e[91m,$e[0m"
 #                    normal     ,green ,yellow,red   ,reset
-
-$env:PACKER_ROOT = "$ROOT/packer".Replace("\", "/")
-$env:PACKER_NO_COLOR = "true"
-$env:PACKER_CACHE_DIR = "$env:PACKER_ROOT/packer_cache"
-$env:PACKER_LOG_PATH = "$env:PACKER_ROOT/_packer.log"
-$env:PACKER_LOG = "0"
