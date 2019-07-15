c92352a4a0e4b39c61057b94552788e98da1f857
stefaanc <stefaan.coussement@gmail.com>
2019-07-15 16:34:45 +0100

    updated psprofile

--- .psprofile.ps1
+++ .psprofile.ps1
@@ -1,18 +1,14 @@
 Set-Variable HOME "$env:USERPROFILE" -Scope Global -Force
 ( Get-PSProvider 'FileSystem' ).Home = $HOME   # replace "~"
 
-$ROOT = "$HOME\Projects\steps"
-$PATH = "$ROOT\scripts;$env:PATH"
-
-$env:HOME = $HOME
-$env:ROOT = $ROOT
-$env:PATH = $PATH
+$global:ROOT = "$HOME\Projects\steps"
+$env:PATH = "$ROOT\scripts;$env:PATH"
 
 if ( -not ( Get-Location ).Path.StartsWith("$ROOT") ) {
     Set-Location "$ROOT"
 }
 
-. Apply-PSConsoleSettings "STEPS"
+Apply-PSConsoleSettings "STEPS"
 
 $e = [char]27
 $env:STEPS_COLORS = "$e[38;5;45m,$e[92m,$e[93m,$e[91m,$e[0m"
